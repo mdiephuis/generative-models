3aca3ebfffdc899b75317a2df38b64aaeff10281
diephuis <Maurits.Diephuis@unige.ch>
2019-11-20 16:05:19 +0100

    AAE cleanup

--- models.py
+++ models.py
@@ -352,18 +352,20 @@ class AAE_Encoder(nn.Module):
         self.latent_size = latent_size
         self.d = d
 
-        self.conv1_1 = nn.Conv2d(self.in_channels, self.d // 2, 4, 2, 1)
-        self.conv2 = nn.Conv2d(self.d // 2, self.d * 2, 4, 2, 1)
-        self.conv2_bn = nn.BatchNorm2d(self.d * 2)
-        self.conv3 = nn.Conv2d(self.d * 2, self.d * 4, 4, 2, 1)
-        self.conv3_bn = nn.BatchNorm2d(self.d * 4)
-        self.conv4 = nn.Conv2d(self.d * 4, self.latent_size, 4, 1, 0)
+        self.network = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.d // 2, 4, 2, 1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(self.d // 2, self.d * 2, 4, 2, 1),
+            nn.BatchNorm2d(self.d * 2),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(self.d * 2, self.d * 4, 4, 2, 1),
+            nn.BatchNorm2d(self.d * 4),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(self.d * 4, self.latent_size, 4, 1, 0)
+        )
 
     def forward(self, x):
-        x = torch.leaky_relu(self.conv1_1(x), 0.2)
-        x = torch.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
-        x = torch.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
-        x = self.conv4(x)
+        x = self.network(x)
         return torch.tanh(x)
 
 
@@ -374,23 +376,23 @@ class AAE_Generator(nn.Module):
         self.latent_size = latent_size
         self.d = d
 
-        self.deconv1_1 = nn.ConvTranspose2d(self.latent_size, self.d * 2, 4, 1, 0)
-        self.deconv1_1_bn = nn.BatchNorm2d(self.d * 2)
-        self.deconv1_2 = nn.ConvTranspose2d(10, self.d * 2, 4, 1, 0)
-        self.deconv1_2_bn = nn.BatchNorm2d(self.d * 2)
-        self.deconv2 = nn.ConvTranspose2d(self.d * 2, self.d * 2, 4, 2, 1)
-        self.deconv2_bn = nn.BatchNorm2d(self.d * 2)
-        self.deconv3 = nn.ConvTranspose2d(self.d * 2, self.d, 4, 2, 1)
-        self.deconv3_bn = nn.BatchNorm2d(self.d)
-        self.deconv4 = nn.ConvTranspose2d(self.d, self.in_channels, 4, 2, 1)
+        self.network = nn.Sequential(
+            nn.ConvTranspose2d(self.latent_size, self.d * 2, 4, 1, 0),
+            nn.BatchNorm2d(self.d * 2),
+            nn.LeakyReLU(0.1),
+            nn.ConvTranspose2d(self.d * 2, self.d * 2, 4, 2, 1),
+            nn.BatchNorm2d(self.d * 2),
+            nn.LeakyReLU(0.1),
+            nn.ConvTranspose2d(self.d * 2, self.d, 4, 2, 1),
+            nn.BatchNorm2d(self.d),
+            nn.LeakyReLU(0.1),
+            nn.ConvTranspose2d(self.d, self.in_channels, 4, 2, 1)
+        )
 
     # forward method
     def forward(self, x):
-        x = torch.relu(self.deconv1_1_bn(self.deconv1_1(x)))
-        x = torch.relu(self.deconv2_bn(self.deconv2(x)))
-        x = torch.relu(self.deconv3_bn(self.deconv3(x)))
-        x = torch.tanh(self.deconv4(x)) * 0.5 + 0.5
-        return x
+        x = self.network(x)
+        return torch.tanh(x) * 0.5 + 0.5
 
 
 class AAE_Discriminator(nn.Module):
@@ -398,16 +400,17 @@ class AAE_Discriminator(nn.Module):
         super(AAE_Discriminator, self).__init__()
         self.latent_size = latent_size
         self.d = d
-
-        self.linear1 = nn.Linear(self.latent_size, self.d)
-        self.linear2 = nn.Linear(self.d, self.d)
-        self.linear3 = nn.Linear(self.d, 1)
+        self.network = nn.Sequential(
+            nn.Linear(self.latent_size, self.d),
+            nn.LeakyReLU(0.2),
+            nn.Linear(self.d, self.d),
+            nn.LeakyReLU(0.2),
+            nn.Linear(self.d, 1)
+        )
 
     def forward(self, x):
-        x = torch.leaky_relu((self.linear1(x)), 0.2)
-        x = torch.leaky_relu((self.linear2(x)), 0.2)
-        x = torch.sigmoid(self.linear3(x))
-        return x
+        x = self.network(x)
+        return torch.sigmoid(x)
 
 
 class AAE_MNIST_Encoder(nn.Module):
@@ -416,13 +419,15 @@ class AAE_MNIST_Encoder(nn.Module):
         self.in_channels = in_channels
         self.latent_size = latent_size
 
-        self.linear1 = nn.Linear(self.in_channels, self.latent_size)
-        self.linear2 = nn.Linear(self.latent_size, self.latent_size)
+        self.network = nn.Sequential(
+            nn.Linear(self.in_channels, self.latent_size),
+            torch.LeakyReLU(0.2),
+            nn.Linear(self.latent_size, self.latent_size)
+        )
 
     def forward(self, x):
-        x = torch.leaky_relu((self.linear1(x)), 0.2)
-        x = torch.tanh((self.linear2(x)))
-        return x
+        x = self.network(x)
+        return torch.tanh(x)
 
 
 class AAE_MNIST_Generator(nn.Module):
@@ -431,12 +436,15 @@ class AAE_MNIST_Generator(nn.Module):
         self.out_channels = out_channels
         self.latent_size = latent_size
 
-        self.linear1 = nn.Linear(self.latent_size, self.out_channels)
-        self.linear2 = nn.Linear(self.out_channels, self.out_channels)
+        self.network = nn.Sequential(
+            nn.Linear(self.latent_size, self.out_channels),
+            torch.LeakyReLU(0.2),
+            nn.Linear(self.out_channels, self.out_channels),
+            torch.LeakyReLU(0.2)
+        )
 
     def forward(self, x):
-        x = torch.leaky_relu((self.linear1(x)), 0.2)
-        x = torch.leaky_relu((self.linear2(x)), 0.2)
+        x = self.network(x)
         return x
 
 
@@ -445,10 +453,12 @@ class AAE_MNIST_Discriminator(nn.Module):
         super(AAE_MNIST_Discriminator, self).__init__()
         self.latent_size = latent_size
 
-        self.linear1 = nn.Linear(self.latent_size, self.latent_size // 2)
-        self.linear2 = nn.Linear(self.latent_size // 2, 1)
+        self.network = nn.Sequential(
+            nn.Linear(self.latent_size, self.latent_size // 2),
+            torch.LeakyReLU(0.2),
+            nn.Linear(self.latent_size // 2, 1)
+        )
 
     def forward(self, x):
-        x = torch.leaky_relu((self.linear1(x)), 0.2)
-        x = torch.sigmoid(self.linear2(x))
-        return x
+        x = self.network(x)
+        return torch.sigmoid(x)
